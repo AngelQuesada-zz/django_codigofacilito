c38a54eacb138798e384f34ea7f8b57c4353822b
Angel Quesada <angelquesadaruiz@gmail.com>
2018-11-20 23:38:22 +0100

    Borrado el anterior proyecto y creado "refugio"
    El tutorial sigue con un proyecto con un nombre distinto así que he borrado el anterior

--- refugio/refugio/urls.py
+++ refugio/refugio/urls.py
@@ -1,4 +1,4 @@
-"""codigofacilito URL Configuration
+"""refugio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
