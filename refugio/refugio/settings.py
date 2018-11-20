f6aee434c181639d7fd6ffa78ae216aced132df8
Angel Quesada <angelquesadaruiz@gmail.com>
2018-11-21 00:34:36 +0100

    Inicializado el proyecto "Refugio" y añadidos archivos al gitignore
    - Se ha inicializado el proyecto "Refugio"
    - Se han añadido los archivos caché al gitignore

--- refugio/refugio/settings.py
+++ refugio/refugio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.mascota',
+    'apps.adopcion',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
