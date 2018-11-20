0bc2eecc3b5f9703f2f287ca3ab4a9230fa215cf
Angel Quesada <angelquesadaruiz@gmail.com>
2018-11-21 00:56:42 +0100

    Añadidos modelos a mascotas y adopción, gitignore...
    - Se han añadido modelos a mascotas y adopción
    - Se han añadido archivos a gitignore

--- refugio/apps/adopcion/models.py
+++ refugio/apps/adopcion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    telefono = models.IntegerField()
+    email = models.EmailField(max_length=254)
+    domicilio = models.TextField()
+
