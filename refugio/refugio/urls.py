40ba80a4e20c191268120b2e040aef7230d898a1
Angel Quesada <angelquesadaruiz@gmail.com>
2018-11-25 23:24:29 +0100

    Configuradas urls y vistas de prueba
    - Se han configurado las primeras URLs para mascotas y adopcion
    - Se han creado vistas de prueba

--- refugio/refugio/urls.py
+++ refugio/refugio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^mascota/', include('apps.mascota.urls')),
+    url(r'^adopcion/', include('apps.adopcion.urls')),
 ]
