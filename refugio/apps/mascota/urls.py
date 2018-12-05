0efebdbf0761e89e6ecf1da491f1a98442bf0a0c
Angel Quesada <angelquesadaruiz@gmail.com>
2018-12-06 00:29:12 +0100

    Mascota: Cambiadas las vistas de listar y crear por clases
    - Se ha cambiado la vista de listar y crear de "funciones" a "clases"

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 
-from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreateView
+
 
 app_name = "mascota"
 
 urlpatterns = [
     path('', index, name='index'),
-    path('nuevo', mascota_view, name='mascota_crear'),
-    path('lista', mascota_list, name='mascota_listar'),
+    path('nuevo', MascotaCreateView.as_view(), name='mascota_crear'),
+    path('lista', MascotaList.as_view(), name='mascota_listar'),
     path('editar/<int:id_mascota>/', mascota_edit, name='mascota_editar'),
     path('eliminar/<int:id_mascota>/', mascota_delete, name='mascota_eliminar'),
 ]
--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView
+from django.urls import reverse, reverse_lazy
 
 from apps.mascota.forms import MascotaForm
 from apps.mascota.models import Mascota
@@ -42,3 +44,15 @@ def mascota_delete(request, id_mascota):
         mascota.delete()
         return redirect('mascota:mascota_listar')
     return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
+
+class MascotaList(ListView):
+    model = Mascota
+    template_name = 'mascota/mascota_list.html'
+
+
+class MascotaCreateView(CreateView):
+    model = Mascota
+    form_class = MascotaForm
+    template_name = "mascota/mascota_form.html"
+    success_url = reverse_lazy('mascota:mascota_listar')
+
