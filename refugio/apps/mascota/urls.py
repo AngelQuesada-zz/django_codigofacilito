b656c6974d85416761a71eb21d3f94c3e697c1bb
Angel Quesada <angelquesadaruiz@gmail.com>
2018-12-05 23:50:00 +0100

    Creado eliminar y editar en mascota
    - Se ha creado la pantalla de eliminación de mascotas
    - Se ha creado la pantalla de edición de mascotas

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 
-from apps.mascota.views import index, mascota_view, mascota_list
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
 
 app_name = "mascota"
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('nuevo', mascota_view, name='mascota_crear'),
     path('lista', mascota_list, name='mascota_listar'),
+    path('editar/<int:id_mascota>/', mascota_edit, name='mascota_editar'),
+    path('eliminar/<int:id_mascota>/', mascota_delete, name='mascota_eliminar'),
 ]
--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -24,3 +24,21 @@ def mascota_list(request):
     mascotas = Mascota.objects.all()
     context = {'mascotas':mascotas}
     return render (request, 'mascota/mascota_list.html', context)
+
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_delete(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'POST':
+        mascota.delete()
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
