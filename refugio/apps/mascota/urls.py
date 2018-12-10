501405c741bb4ab442b88b786892b50c44ba00e9
Angel Quesada <angelquesadaruiz@gmail.com>
2018-12-10 22:51:34 +0100

    Mascota: Creadas vistas para editar y para eliminar
    - Se ha creado la vista de eliminar
    - Se ha creado la vista de editar

--- refugio/apps/mascota/urls.py
+++ refugio/apps/mascota/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 
-from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreateView
+from apps.mascota.views import *
 
 
 app_name = "mascota"
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('nuevo', MascotaCreateView.as_view(), name='mascota_crear'),
     path('lista', MascotaList.as_view(), name='mascota_listar'),
-    path('editar/<int:id_mascota>/', mascota_edit, name='mascota_editar'),
-    path('eliminar/<int:id_mascota>/', mascota_delete, name='mascota_eliminar'),
+    path('editar/<int:pk>/', MascotaUpdateView.as_view(), name='mascota_editar'),
+    path('eliminar/<int:pk>/', MascotaDeleteView.as_view(), name='mascota_eliminar'),
 ]
--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 
 from apps.mascota.forms import MascotaForm
@@ -49,10 +49,20 @@ class MascotaList(ListView):
     model = Mascota
     template_name = 'mascota/mascota_list.html'
 
-
 class MascotaCreateView(CreateView):
     model = Mascota
     form_class = MascotaForm
     template_name = "mascota/mascota_form.html"
     success_url = reverse_lazy('mascota:mascota_listar')
 
+class MascotaUpdateView(UpdateView):
+    model = Mascota
+    form_class = MascotaForm
+    template_name = "mascota/mascota_form.html"
+    success_url = reverse_lazy('mascota:mascota_listar')
+
+
+class MascotaDeleteView(DeleteView):
+    model = Mascota
+    template_name = "mascota/mascota_delete.html"
+    success_url = reverse_lazy('mascota:mascota_listar')
